4e23ff1ed9a0ecb1a2670074a8d778ba86932381
xyc <ycworkin@163.com>
2021-01-29 16:11:03 +0800

    feat: init

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/yc/Desktop/github/flutter-sdk"
+export "FLUTTER_APPLICATION_PATH=/Users/yc/Desktop/github/flutter-hello-world"
+export "FLUTTER_TARGET=/Users/yc/Desktop/github/flutter-hello-world/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/yc/Desktop/github/flutter-hello-world/.dart_tool/package_config.json"
